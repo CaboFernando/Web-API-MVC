c0b081cdbdd3a307cfbaeeb6ac834e97d9632338
CaboFernando <fer.1972@hotmail.com>
2022-05-06 17:31:44 -0300

    Add project files.

---
+++ Web-API-MVC/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web_API_MVC.Models
+{
+    [Table("Categoria")]
+    public class Categoria
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+    }
+}
